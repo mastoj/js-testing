3c19f01c68b3a5fcd186491d258ffff79ac3a142
Tomas Jansson <tomas.jansson@gmail.com>
2013-05-03 20:37:55 +0200

    something

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
   var BEKK = window.BEKK = {};
 
   BEKK.start = function() {
-    // kalles av index.html
-    // her kan vi (etterhvert) starte applikasjonen
+    var user = new BEKK.User({name: "Vegard"});
+    var view = new BEKK.UserView({user: user.toJSON(), el: $("#profile")});
+    view.render();
   };
 
   BEKK.UserView = Backbone.View.extend({
@@ -18,8 +19,22 @@
 
     initialize: function(options) {
       this.user = options.user;
+      this.user.on("change", this.render, this);
+    },
+
+    render: function() {
+      var html = Mustache.to_html(this.template, this.user.toJSON());
+      this.$el.html(html);
+      return this;
     }
   });
+
+  BEKK.User = Backbone.Model.extend({
+    url: function() {
+      return "https://api.twitter.com/1/users/show.json?screen_name=" + this.get('username');
+    }
+  });
+
 
   BEKK.FriendsView = Backbone.View.extend({});
 
--- test/spec/userViewSpec.js
+++ test/spec/userViewSpec.js
@@ -1,19 +1,14 @@
 describe("The user view", function(){
-
-    it("should have a user when initialized", function(){
-        var user = {};
-
-        // ...
-
-        expect(view.user).toBeDefined();
-    });
-
-    xit("should show user info when rendered", function(){
-        var user = {
-            name: "Test Testesen"
-        };
-
-        // ...
+    it("should show user info when rendered", function(){
+        var data = '{"id":24046215,"id_str":"24046215","name":"Tomas Jansson","screen_name":"TomasJansson","location":"Oslo, Norway"}';
+        var view, user;
+        fakeResponse(data, {}, function() {
+            user = new BEKK.User({username: "TomasJansson"});
+            view = new BEKK.UserView({user: user, el: $("<ul></ul>")});
+            user.fetch();
+            user.trigger("change");
+        })
+        expect(view.$("h2")).toHaveText("Tomas Jansson");
     });
 
     xit("should update view when monolog is added", function() {
